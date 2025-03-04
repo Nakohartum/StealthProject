6ad4e1b2c5f43bd38953758f7350b40ec1692b85
Nakohartum <58501792+Nakohartum@users.noreply.github.com>
2025-03-04 09:45:58 +0500

    Added MainMenu

--- Assets/_Root/Code/QuestFeature/Model/KillQuestPart.cs
+++ Assets/_Root/Code/QuestFeature/Model/KillQuestPart.cs
@@ -1,9 +1,14 @@
-﻿namespace _Root.Code.QuestFeature.Model
+﻿using UnityEngine;
+
+namespace _Root.Code.QuestFeature.Model
 {
     public class KillQuestPart : QuestPart
     {
-        public KillQuestPart(string description, bool isDone, string targetID) : base(description, isDone, targetID)
+        private int _amount;
+        private int _currentAmount = 0;
+        public KillQuestPart(string description, bool isDone, string targetID, int amount) : base(description, isDone, targetID)
         {
+            _amount = amount;
         }
 
         
@@ -11,6 +16,11 @@
         public override void CheckWhetherDone(string targetId)
         {
             if (this.TargetID == targetId)
+            {
+                _currentAmount = Mathf.Min(_currentAmount + 1, _amount);
+            }
+
+            if (_currentAmount == _amount)
             {
                 IsDone = true;
             }
